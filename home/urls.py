bbf295424b5dc743076d79a602693546a22d73c8
Salvador Aolani <trabajolasc27@gmail.com>
2026-09-24 13:50:44 -0600

    Crea detalle, categorias, inicio de sesion y registro

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,8 @@ app_name = "home"
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("noticia/", views.detalle_noticia, name="detalle"),
+    path("categoria/", views.categoria, name="categoria"),
+    path("iniciar-sesion/", views.login_view, name="login"),
+    path("registro/", views.registro, name="registro"),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "home/index.html")
+
+
+def detalle_noticia(request):
+    return render(request, "home/detalle.html")
+
+
+def categoria(request):
+    return render(request, "home/categoria.html")
+
+
+def login_view(request):
+    return render(request, "home/login.html")
+
+
+def registro(request):
+    return render(request, "home/registro.html")
